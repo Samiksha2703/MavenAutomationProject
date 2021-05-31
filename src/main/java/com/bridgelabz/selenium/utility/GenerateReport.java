85ca94277fecbff9a4bd19b010ae6e8d6319e7f3
Samiksha <shende.samiksha@rediffmail.com>
2021-05-31 13:20:25 +0530

    [Samiksha].Add code to send email with selenium

---
+++ src/main/java/com/bridgelabz/selenium/utility/GenerateReport.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.selenium.utility;
+
+import org.automationtesting.excelreport.Xl;
+
+public class GenerateReport {
+
+    public static void main(String[] args) throws Exception {
+
+         Xl.generateReport("C:\\Users\\kalam\\ExcelReport","excel-report.xlsx");
+    }
+}
---
+++ src/test/java/com/bridgelabz/selenium/test/excelreport/TestClassTwo.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.selenium.test.excelreport;
+
+import com.bridgelabz.selenium.base.Base;
+import com.bridgelabz.selenium.pages.Login;
+import com.bridgelabz.selenium.pages.Logout;
+import com.bridgelabz.selenium.pages.Search;
+import com.bridgelabz.selenium.test.TestCases;
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+
+public class TestClassTwo extends Base {
+
+    static Logger log = Logger.getLogger(TestCases.class.getName());
+    public Search search;
+    public Login login;
+
+    //Method to test the search feature functionality
+    @Test(priority = 1)
+    public void search_Item() {
+        search = new Search(driver);
+        search.searchItem();
+        log.info("This is log message for test case : search_Item");
+    }
+
+        //Method to test the login feature functionality
+    @Test(priority = 2)
+    public void amazon_Login() throws InterruptedException {
+        login = new Login(driver);
+        login.loggedInToApp("shende.samiksha@rediffmail.com", "Welcome@01");
+        log.info("This is log message for test case : login into account");
+    }
+
+    //Method to test the functionality of the SignOut feature
+    @Test(priority = 3)
+    public void select_Logout() {
+        Logout logout = new Logout(driver);
+        logout.signOut_From_Account();
+        log.info("This is log message for test case : logout from account");
+    }
+}
